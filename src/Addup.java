7da6bd2f64020026c283cad569e4d458af10e7db
nadinyates <nadinyates@users.noreply.github.com>
2017-12-25 23:45:34 +0100

    Initial commit. Not sure if I should be including the eclipse project stuff or just bin/ and src/

---
+++ src/Addup.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Addup {
+	public static void main(String[] args) {
+		// add two numbers together
+		 Scanner num  = new Scanner(System.in); 
+		 System.out.print("Enter your num1: ");
+		 int num1 = num. nextInt();
+		 System.out.print("Enter your num2: ");
+		 int num2 = num. nextInt();
+		 int total = num2 + num1;
+		 System.out.print("Total: " + total);
+	}
+}
